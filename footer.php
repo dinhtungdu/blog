64821d04bf1a25b17cc4fc0700e78c8a759bcf45
Tung Du <dinhtungdu@gmail.com>
2021-01-24 12:23:01 +0700

    wip: page header

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
 	</main><!-- #content -->
-	<?php render_view( 'footer.twig', [
+	<?php
+	render_view( 'footer.twig', [
 		'year' => date('Y'),
 		'homeUrl' => home_url(),
 		'blogName' => get_bloginfo( 'name' ),
-	] ); ?>
+	] );
+	?>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
